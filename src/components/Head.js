84c223f266c96f7d8d62831fc8745f6ea4b52158
Steak <lakhotiya.sil07@gmail.com>
2023-12-02 23:36:37 +0530

    Initial setup

---
+++ src/components/Head.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Head = () => {
+  return (
+    <div>
+        {/* 3 sections - hamburger + logo , search , userinfo section */}
+
+        
+    </div>
+  )
+}
+
+export default Head
